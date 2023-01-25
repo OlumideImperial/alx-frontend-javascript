c0231bbc24fa029494d0abb357a53d606fec8ca7
Olumide Imperial <imperialmidez@gmail.com>
2023-01-25 13:22:33 -0800

    0x01-ES6_promise

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,17 @@
+
+
+rt default function guardrail(mathFunction) {
+  const queue = [];
+  let value;
+
+  try {
+    value = mathFunction();
+  } catch (err) {
+    value = err.toString();
+  }
+
+  queue.push(value);
+  queue.push('Guardrail was processed');
+
+  return queue;
+}
---
+++ 0x01-ES6_promise/README.md
@@ -0,0 +1,1 @@
+This readme is for 0x01-ES6_promise
